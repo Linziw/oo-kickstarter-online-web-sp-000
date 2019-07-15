62ecdae3baaf97b602e0431eedf889e3729518fc
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-15 12:38:33 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Backer
   @@all = []
   
@@ -11,6 +13,7 @@ class Backer
   
   def back_project(project)
     @backed_projects << Project.find_by_project(project)
+    binding.pry
   end
   
   def self.find_by_name(backer)
