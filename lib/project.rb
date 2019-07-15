0391a6ee247a1253802abb2b279d980e7f0ddc80
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-15 12:33:15 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,17 +1,21 @@
 class Project
   attr_accessor :title, :backers
   
+  @@all
+  
+  
   def initialize(title)
     @title=title
     @backers = []
+    @@all << self
   end
   
   def add_backer(backer)
     @backers << Backer.find_by_name(backer)
   end
   
-   def self.find_by_name(backer)
-    @@all.find {|backer| backer.name == backer}
+   def self.find_by_title(title)
+    @@all.find {|title| .name == backer}
   end
   
   
